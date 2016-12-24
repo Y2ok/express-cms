bf94cd0394f0076a1e86260eef420d17b02f9126
Valters Valdersteins <valters.valdersteins@gmail.com>
2016-12-24 11:54:47 +0200

    login route added

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({ message: 'hooray! welcome to our api!' });  
 });
 
 module.exports = router;
